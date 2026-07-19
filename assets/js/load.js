e7c66397718c57bc3524f7728f6d5d3f2fde2f15
Abdallahweb <121055533+Abdallahweb@users.noreply.github.com>
2026-07-19 09:33:27 +0300

    Add files via upload

--- assets/js/load.js
+++ assets/js/load.js
@@ -12,3 +12,39 @@ $( ".filter-active" ).trigger( "click" );
 
 }
 
+
+document.onkeydown = function(e) {
+        if (e.ctrlKey && 
+            (e.keyCode === 67 || 
+             e.keyCode === 86 || 
+             e.keyCode === 85 || 
+             e.keyCode === 117)) {
+            return false;
+        } else {
+            return true;
+        }
+};
+$(document).keypress("u",function(e) {
+  if(e.ctrlKey)
+  {
+return false;
+}
+else
+{
+return true;
+}
+});
+
+document.addEventListener("contextmenu", function (e){
+    e.preventDefault();
+}, false);
+
+
+$(document).keydown(function (event) {
+    if (event.keyCode == 123) { // Prevent F12
+        return false;
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
+        return false;
+    }
+});
+
