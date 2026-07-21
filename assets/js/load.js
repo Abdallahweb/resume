3d503172c19604da410f89d1d0e9a0700fe2efd5
Abdallahweb <121055533+Abdallahweb@users.noreply.github.com>
2026-07-21 07:00:07 +0300

    Add files via upload

--- assets/js/load.js
+++ assets/js/load.js
@@ -6,7 +6,7 @@ loadingVar = setTimeout(showPage, 1000);
 }
 
 function showPage() {
-document.getElementById("loader").classList.add("zoomOut");;
+document.getElementById("loader").classList.add("zoomOut");
 document.getElementById("loadingDiv").style.display = "block";
 $( ".filter-active" ).trigger( "click" );
 
